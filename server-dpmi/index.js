edbe428e6a7c1eb50d8ab935b5f1da6185e2b119
PamanAleph <alieffirmandabisnis@gmail.com>
2024-09-18 20:52:18 +0700

    update

--- server-dpmi/index.js
+++ server-dpmi/index.js
@@ -1,2 +1,11 @@
-const express = require(express)
-const app = express()
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
